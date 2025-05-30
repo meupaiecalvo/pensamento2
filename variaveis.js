02f5d02c3d8b5cacc4901157df1d2e10a76ac312
meupaiecalvo <126251896+meupaiecalvo@users.noreply.github.com>
2025-05-30 09:38:02 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,14 @@
+// declarando variáveis
+
+var num1 = 10;
+var num2 = 8;
+var num3 = -5;
+
+soma = (num1 - num3) * (num1 + num2);
+
+console.log("A soma dos númwros é:" + soma);
+
+// SOMA = +;
+// SUBTRAÇÃO = -;
+// MULTIPLICAÇÃO = *
+// DIVISÃO = /
